0426f01a5912cc04fb3985cd7a6cb6def77f9fca
Joyonta Saha <joyontasaha8@gmail.com>
2021-07-31 21:44:27 +0600

    @Transactional, @Slf4j annotations added, log.info() with required message statement added for override methods

--- src/main/java/com/joyonta/springbootsecuritywithjwtaccessrefreshtokens/service/UserServiceImpl.java
+++ src/main/java/com/joyonta/springbootsecuritywithjwtaccessrefreshtokens/service/UserServiceImpl.java
@@ -5,28 +5,35 @@ import com.joyonta.springbootsecuritywithjwtaccessrefreshtokens.domain.User;
 import com.joyonta.springbootsecuritywithjwtaccessrefreshtokens.repo.RoleRepo;
 import com.joyonta.springbootsecuritywithjwtaccessrefreshtokens.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
+@Slf4j
 public class UserServiceImpl implements UserService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
 
     @Override
     public User saveUser(User user) {
+        log.info("Saving user {} to the database", user.getName());
         return userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving role {} to the database", role.getName());
         return roleRepo.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {} ", roleName, username);
         User user = userRepo.findByUserName(username);
         Role role = roleRepo.findByName(roleName);
 
@@ -35,11 +42,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String username) {
+        log.info("Fetching user {} ", username);
         return userRepo.findByUserName(username);
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("Fetching all users");
         return userRepo.findAll();
     }
 }
