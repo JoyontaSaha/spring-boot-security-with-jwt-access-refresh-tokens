35fc673b6d1ba831b0eb4fae4d30d2a7a8a708bd
Joyonta Saha <joyontasaha8@gmail.com>
2021-07-31 20:49:03 +0600

    initial commit

---
+++ src/main/java/com/joyonta/springbootsecuritywithjwtaccessrefreshtokens/SpringBootSecurityWithJwtAccessRefreshTokensApplication.java
@@ -0,0 +1,13 @@
+package com.joyonta.springbootsecuritywithjwtaccessrefreshtokens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityWithJwtAccessRefreshTokensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityWithJwtAccessRefreshTokensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joyonta/springbootsecuritywithjwtaccessrefreshtokens/SpringBootSecurityWithJwtAccessRefreshTokensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyonta.springbootsecuritywithjwtaccessrefreshtokens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityWithJwtAccessRefreshTokensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
